8a49b097f5185e3683ce16a3e83e7f954d03d281
beto86 <beto.brava86@gmail.com>
2023-06-08 17:44:38 -0300

    feito conteudo dinamico

--- core/views.py
+++ core/views.py
@@ -1,21 +1,21 @@
 from django.views.generic import TemplateView
-# from django.urls import reverse_lazy
-# from django.contrib import messages
+from django.urls import reverse_lazy
+from django.contrib import messages
 
-# from .models import Servico, Funcionario
-# from .forms import ContatoForm
+from .models import Servico, Funcionario
+#from .forms import ContatoForm
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
-    # form_class = ContatoForm
-    # success_url = reverse_lazy('index')
+    #form_class = ContatoForm
+    #success_url = reverse_lazy('index')
 
-    # def get_context_data(self, **kwargs):
-    #     context = super(IndexView, self).get_context_data(**kwargs)
-    #     context['servicos'] = Servico.objects.order_by('?').all()
-    #     context['funcionarios'] = Funcionario.objects.order_by('?').all()
-    #     return context
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['servicos'] = Servico.objects.order_by('?').all()
+        context['funcionarios'] = Funcionario.objects.order_by('?').all()
+        return context
 
     # def form_valid(self, form, *args, **kwargs):
     #     form.send_mail()
